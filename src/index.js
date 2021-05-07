abd202209011386643613b22a8d190b70fdeb4ab
Pedro Rodriguez Alia <pedro.rodalia@gmail.com>
2021-05-07 18:38:36 +0200

    feat: Boilerplate cleanup and new file structure

--- src/index.js
+++ src/index.js
@@ -1,14 +1,14 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./assets/index.css";
+import IomMainView from "./views/iom-main-view/IomMainView";
+import reportWebVitals from "./utils/reportWebVitals";
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <IomMainView />
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
 
 // If you want to start measuring performance in your app, pass a function
---
+++ src/views/iom-main-view/IomMainView.js
@@ -0,0 +1,11 @@
+import "./IomMainView.css";
+
+function IomMainView() {
+  return (
+    <section className="main">
+      <header className="header">Hello World!</header>
+    </section>
+  );
+}
+
+export default IomMainView;
